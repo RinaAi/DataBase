fe61729f93442fa8e37835c4ae03be45819b93ce
RinaAi <RinaAi@users.noreply.github.com>
2019-04-28 19:05:00 +0500

    1

--- DataBase/Program.cs
+++ DataBase/Program.cs
@@ -1,24 +1,112 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
+using Dapper;
+using Npgsql;
 
 namespace DataBase
 {
-    public class Program
+    class Program
     {
-        public static void Main(string[] args)
+        static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            PrepareDatabase();
+
+            using (var conn = CreateConnection("cats"))
+            {
+                Process(conn);
+            }
+            
+            Console.WriteLine("Press <Enter> to drop database");
+            Console.Read();
+            
+            DropDatabase();
         }
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+        private static void Process(NpgsqlConnection conn)
+        {
+            // дока по дапперу https://github.com/StackExchange/Dapper
+
+            conn.Execute("create table cats (id int, name text, size text, bread text, fluffiness text)");
+
+            conn.Execute("insert into cats(id, name, size, bread, fluffiness) values(:id, :name, :size, :bread, :fluffiness)",
+                new
+                {
+                    id = 1,
+                    name = "Умка",
+                    size = "Не очень большой",
+                    bread = "Булочка",
+                    fluffiness = "Низкая"
+                }
+            );
+
+            conn.Execute(
+                "insert into cats(id, name, size, bread, fluffiness) values(:id, :name, :size, :bread, :fluffiness)",
+                new
+                {
+                    id = 2,
+                    name = "Себастьян",
+                    size = "Большой",
+                    bread = "Батон",
+                    fluffiness = "Слишком высокая"
+                }
+            );
+
+            IEnumerable<Cat> cats = conn.Query<Cat>("select id, name, size, bread, fluffiness from cats");
+
+            foreach (var cat in cats)
+            {
+                Console.WriteLine("{0,-5} {1,-80} {2, -40} {3, 1} {4, 20} {4}", cat.Id, cat.Name, cat.Size, cat.Bread, 
+                    cat.Fluffiness);
+            }
+        }
+
+        private static void DropDatabase()
+        {
+            using (var conn = CreateConnection("postgres"))
+            {
+                conn.Execute("drop database cats");
+            }
+        }
+
+        private static void PrepareDatabase()
+        {
+            using (var conn = CreateConnection("postgres"))
+            {
+                conn.Execute("create database cats");
+            }
+        }
+
+        private static NpgsqlConnection CreateConnection(string db)
+        {
+            // Все возможные параметрты https://www.npgsql.org/doc/connection-string-parameters.html
+            var conn = new NpgsqlConnection($"server=localhost;userid=postgres;database={db};Pooling=false");
+
+            conn.Open();
+            
+            return conn;
+        }
+    }
+
+
+    public class Cat
+    {
+        public Cat(int id, string name, string size, string bread, string fluffiness)
+        {
+            Id = id;
+            Name = name;
+            Size = size;
+            Bread = bread;
+            Fluffiness = fluffiness;
+        }
+
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Size { get; set; }
+
+        public string Bread { get; set; }
+
+        public string Fluffiness { get; set; }
     }
 }
